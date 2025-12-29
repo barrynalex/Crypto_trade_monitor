d56c2cbef74a0fc78bf6bf8b716ee4d9a8837eef
Barry Sun <barrynalex0105@gmail.com>
2025-12-29 17:37:11 +0800

    file name bug fix

--- scripts/deploy_aws.sh
+++ scripts/deploy_aws.sh
@@ -11,14 +11,14 @@ curl -SL https://github.com/docker/compose/releases/download/v2.24.7/docker-comp
 
 # Get repo
 su - ec2-user -c "git clone https://github.com/barrynalex/Crypto_trade_monitor.git"
-cd /home/ec2-user/crypto_trade_monitor
+cd /home/ec2-user/Crypto_trade_monitor
 
 # 取得 AWS Public IP
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 PUBLIC_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4`
 
 # 建立 .env 檔案，讓 docker-compose 讀取
-echo "EC2_PUBLIC_IP=$PUBLIC_IP" > /home/ec2-user/crypto_trade_monitor/.env
+echo "EC2_PUBLIC_IP=$PUBLIC_IP" > /home/ec2-user/Crypto_trade_monitor/.env
 
 # Optional: pull env/creds from SSM or Secrets Manager here
 # Run stack
